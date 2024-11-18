71123a11cb83672ccd63bb153766c01019abe9c6
Mikhail <hramtsov13@mail.ru>
2024-11-18 11:57:56 +0500

    feat(main): deleted unused header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,4 @@
-<script setup lang="ts">
-// import { NAV_ITEMS } from '@/constants/nav';
-// import { cn } from '@/lib/utils';
-
-// const route = useRoute();
-
-// const isLinkActive = (link: string) => {
-//   return link === route.path;
-// };
-</script>
-
 <template>
-  <!-- <header class="bg-slate-900">
-    <div class="container">
-      <nav class="space-x-4 py-6">
-        <NuxtLink
-          v-for="link in NAV_ITEMS"
-          :key="link.link"
-          :href="link.link"
-          :aria-label="`Link to ${link.title} page`"
-          :class="cn('font-light text-white underline', { 'text-muted-foreground': isLinkActive(link.link) })"
-        >
-          {{ link.title }}
-        </NuxtLink>
-      </nav>
-    </div>
-  </header> -->
-
   <div class="container py-10">
     <slot />
   </div>
